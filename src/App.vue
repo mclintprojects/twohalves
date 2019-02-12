b0bcffd7938036c48f5b02acc2676b774b161c55
Mbah Clinton <clintonmbah44@gmail.com>
2019-02-12 07:30:35 +0000

    started revamping the homepage

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,56 @@
 <template>
   <div>
-    <router-view />
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-	created() {
-		this.$store.dispatch('setUsername', localStorage.getItem('username'));
-	}
+  created() {
+    this.$store.dispatch("setUsername", localStorage.getItem("username"));
+  }
 };
 </script>
 
 <style lang="scss">
+@import "colors";
+:root {
+  font-size: 62.5%;
+}
+
 * {
-	margin: 0;
-	padding: 0;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 body {
-	font-family: 'Muli', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	min-height: 100vh;
+  font-family: "Oxygen", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
 }
 
-.app {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	height: 100vh;
+a {
+  text-decoration: none;
 }
 
-.loader {
-	background: tomato;
-	border-radius: 50%;
-	padding: 4px;
-	margin: auto;
+.container {
+  width: 100vw;
+  height: 100vh;
 }
 
-.btn {
-	color: white;
-	background: #2c3e50;
-	border: 0px solid transparent;
-	border-radius: 30px;
-	padding: 16px 24px;
-	text-transform: uppercase;
-	font-size: 12px;
+.button {
+  background: $colorPrimary;
+  padding: 0.8rem 1.6rem;
+  border-radius: 3rem;
+  color: white;
+  font-size: 1.1rem;
+  text-transform: uppercase;
+  font-weight: bold;
 
-	&:hover {
-		background: #1d2b38;
-		cursor: pointer;
-	}
-
-	&:disabled {
-		background: gray;
-		cursor: not-allowed;
-	}
-
-	&:disabled:hover {
-		background: rgb(97, 97, 97);
-	}
-}
-
-.btn-small {
-	padding: 10px;
-	font-size: 10px;
-}
-
-.findingLoader {
-	font-size: 18px;
-	animation: findingAnim 2s infinite;
-}
-
-.input {
-	border: 1px solid #2c3e50;
-	border-radius: 30px;
-	background: white;
-	padding: 16px;
-}
-
-@keyframes findingAnim {
-	from {
-		opacity: 1;
-	}
-
-	to {
-		opacity: 0;
-	}
+  &:hover {
+    background: lighten($colorPrimary, 10);
+  }
 }
 </style>
